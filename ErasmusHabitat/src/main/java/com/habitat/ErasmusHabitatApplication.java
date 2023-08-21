ee01d4ac50d7c9e1ff5d9fa321364a0e066dc18d
atilaeldeveloper <alejo1996.96@gmail.com>
2023-08-21 15:07:38 +0200

    Initial Commit

---
+++ ErasmusHabitat/src/main/java/com/habitat/ErasmusHabitatApplication.java
@@ -0,0 +1,13 @@
+package com.habitat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusHabitatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasmusHabitatApplication.class, args);
+	}
+
+}
---
+++ ErasmusHabitat/src/test/java/com/habitat/ErasmusHabitatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasmusHabitatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
